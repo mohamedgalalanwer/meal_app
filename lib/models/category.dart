04d23bbc31f9cd0c22954bc1756c5b0611f32ce4
Mohamed Galal <mohamedjalalanwer@outlook.com>
2021-03-21 12:14:22 +0200

    Initial commit

---
+++ lib/models/category.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class Categories{
+  final String id;
+  final String title;
+  final Color color;
+
+  const Categories(
+      {@required this.id, @required this.title, this.color = Colors.orange});
+}
